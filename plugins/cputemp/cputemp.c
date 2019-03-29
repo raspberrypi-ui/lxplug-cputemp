098ee7221a97f7812983bff5f226f8bbda6edd11
Simon Long <simon@raspberrypi.org>
2019-03-29 08:00:49 +0000

    Add throttling detection

--- plugins/cputemp/cputemp.c
+++ plugins/cputemp/cputemp.c
@@ -64,12 +64,15 @@ typedef gint (*GetTempFunc) (char const *);
 
 typedef struct
 {
-    GdkColor foreground_color;			    /* Foreground color for drawing area */
-    GdkColor background_color;			    /* Background color for drawing area */
+    GdkColor foreground_color;			    /* Foreground colour for drawing area */
+    GdkColor background_color;			    /* Background colour for drawing area */
+    GdkColor low_throttle_color;			/* Colour for bars with ARM freq cap */
+    GdkColor high_throttle_color;			/* Colour for bars with throttling */
     GtkWidget *da;				            /* Drawing area */
     cairo_surface_t *pixmap;				/* Pixmap to be drawn on drawing area */
     guint timer;				            /* Timer for periodic update */
     float *stats_cpu;			            /* Ring buffer of temperature values */
+    int *stats_throttle;                    /* Ring buffer of throttle status */
     unsigned int ring_cursor;			    /* Cursor for ring buffer */
     guint pixmap_width;				        /* Width of drawing area pixmap; also size of ring buffer; does not include border size */
     guint pixmap_height;			        /* Height of drawing area pixmap; does not include border size */
@@ -156,25 +159,25 @@ static gint sysfs_get_temperature (char const *sensor_path)
     return _get_reading (sstmp);
 }
 
-static gint hwmon_get_temperature(char const *sensor_path)
+static gint hwmon_get_temperature (char const *sensor_path)
 {
     if (sensor_path == NULL) return -1;
     return _get_reading (sensor_path);
 }
 
 
-static int add_sensor (CPUTempPlugin* th, char const* sensor_path, GetTempFunc get_temp)
+static int add_sensor (CPUTempPlugin* c, char const* sensor_path, GetTempFunc get_temp)
 {
-    if (th->numsensors + 1 > MAX_NUM_SENSORS)
+    if (c->numsensors + 1 > MAX_NUM_SENSORS)
     {
         g_message ("cputemp: Too many sensors (max %d), ignoring '%s'",
             MAX_NUM_SENSORS, sensor_path);
         return -1;
     }
 
-    th->sensor_array[th->numsensors] = g_strdup (sensor_path);
-    th->get_temperature[th->numsensors] = get_temp;
-    th->numsensors++;
+    c->sensor_array[c->numsensors] = g_strdup (sensor_path);
+    c->get_temperature[c->numsensors] = get_temp;
+    c->numsensors++;
 
     g_message ("cputemp: Added sensor %s", sensor_path);
 
@@ -182,7 +185,7 @@ static int add_sensor (CPUTempPlugin* th, char const* sensor_path, GetTempFunc g
 }
 
 
-static gboolean try_hwmon_sensors (CPUTempPlugin* th, const char *path)
+static gboolean try_hwmon_sensors (CPUTempPlugin* c, const char *path)
 {
     GDir *sensorsDirectory;
     const char *sensor_name;
@@ -210,7 +213,7 @@ static gboolean try_hwmon_sensors (CPUTempPlugin* th, const char *path)
                 fclose (fp);
             }
             snprintf (sensor_path, sizeof (sensor_path), "%s/%s", path, sensor_name);
-            add_sensor (th, sensor_path, hwmon_get_temperature);
+            add_sensor (c, sensor_path, hwmon_get_temperature);
             found = TRUE;
         }
     }
@@ -218,24 +221,24 @@ static gboolean try_hwmon_sensors (CPUTempPlugin* th, const char *path)
     return found;
 }
 
-static void find_hwmon_sensors (CPUTempPlugin* th)
+static void find_hwmon_sensors (CPUTempPlugin* c)
 {
     char dir_path[100];
-    char *c;
+    char *cptr;
     int i; /* sensor type num, we'll try up to 4 */
 
     for (i = 0; i < 4; i++)
     {
         snprintf (dir_path, sizeof (dir_path), "/sys/class/hwmon/hwmon%d/device", i);
-        if (try_hwmon_sensors (th, dir_path)) continue;
+        if (try_hwmon_sensors (c, dir_path)) continue;
         /* no sensors found under device/, try parent dir */
-        c = strrchr (dir_path, '/');
-        *c = '\0';
-        try_hwmon_sensors (th, dir_path);
+        cptr = strrchr (dir_path, '/');
+        *cptr = '\0';
+        try_hwmon_sensors (c, dir_path);
     }
 }
 
-static void find_sensors (CPUTempPlugin* th, char const* directory, char const* subdir_prefix, GetTempFunc get_temp)
+static void find_sensors (CPUTempPlugin* c, char const* directory, char const* subdir_prefix, GetTempFunc get_temp)
 {
     GDir *sensorsDirectory;
     const char *sensor_name;
@@ -252,29 +255,29 @@ static void find_sensors (CPUTempPlugin* th, char const* directory, char const* 
             if (strncmp (sensor_name, subdir_prefix, strlen (subdir_prefix)) != 0)  continue;
         }
         snprintf (sensor_path, sizeof (sensor_path), "%s%s/", directory, sensor_name);
-        add_sensor (th, sensor_path, get_temp);
+        add_sensor (c, sensor_path, get_temp);
     }
     g_dir_close (sensorsDirectory);
 }
 
-static void check_sensors (CPUTempPlugin *th)
+static void check_sensors (CPUTempPlugin *c)
 {
     int i;
 
-    for (i = 0; i < th->numsensors; i++) g_free (th->sensor_array[i]);
-    th->numsensors = 0;
+    for (i = 0; i < c->numsensors; i++) g_free (c->sensor_array[i]);
+    c->numsensors = 0;
 
-    find_sensors (th, PROC_THERMAL_DIRECTORY, NULL, proc_get_temperature);
-    find_sensors (th, SYSFS_THERMAL_DIRECTORY, SYSFS_THERMAL_SUBDIR_PREFIX, sysfs_get_temperature);
-    if (th->numsensors == 0) find_hwmon_sensors (th);
+    find_sensors (c, PROC_THERMAL_DIRECTORY, NULL, proc_get_temperature);
+    find_sensors (c, SYSFS_THERMAL_DIRECTORY, SYSFS_THERMAL_SUBDIR_PREFIX, sysfs_get_temperature);
+    if (c->numsensors == 0) find_hwmon_sensors (c);
     
-    g_message ("cputemp: Found %d sensors", th->numsensors);
+    g_message ("cputemp: Found %d sensors", c->numsensors);
 }
 
 /* Redraw after timer callback or resize. */
-static void redraw_pixmap(CPUTempPlugin * c)
+static void redraw_pixmap (CPUTempPlugin * c)
 {
-    GdkColor col;
+    GdkColor col, t1col, t2col;
     cairo_t * cr = cairo_create(c->pixmap);
     GtkStyle * style = gtk_widget_get_style(c->da);
     cairo_set_line_width (cr, 1.0);
@@ -292,12 +295,24 @@ static void redraw_pixmap(CPUTempPlugin * c)
     col.red = c->foreground_color.blue;
     col.green = c->foreground_color.green;
     col.blue = c->foreground_color.red;
-    gdk_cairo_set_source_color (cr, &col);
+    t1col.red = c->low_throttle_color.blue;
+    t1col.green = c->low_throttle_color.green;
+    t1col.blue = c->low_throttle_color.red;
+    t2col.red = c->high_throttle_color.blue;
+    t2col.green = c->high_throttle_color.green;
+    t2col.blue = c->high_throttle_color.red;
     for (i = 0; i < c->pixmap_width; i++)
     {
         /* Draw one bar of the CPU usage graph. */
         if (c->stats_cpu[drawing_cursor] != 0.0)
         {
+            if (c->stats_throttle[drawing_cursor] & 0x4)
+                gdk_cairo_set_source_color (cr, &t2col);
+            else if (c->stats_throttle[drawing_cursor] & 0x2)
+                gdk_cairo_set_source_color (cr, &t1col);
+            else
+                gdk_cairo_set_source_color (cr, &col);
+
             float val = c->stats_cpu[drawing_cursor] * 100.0;
             val -= TEMP_LOW;
             val /= TEMP_RANGE;
@@ -329,7 +344,7 @@ static void redraw_pixmap(CPUTempPlugin * c)
     cairo_select_font_face (cr, "monospace", CAIRO_FONT_SLANT_NORMAL, CAIRO_FONT_WEIGHT_NORMAL);
     cairo_set_font_size (cr, fontsize);
     cairo_set_source_rgb (cr, 0, 0, 0);
-    cairo_move_to (cr, (c->pixmap_width >> 1) - ((fontsize * 5) / 3), ((c->pixmap_height + fontsize) >> 1) - 1);
+    cairo_move_to (cr, (c->pixmap_width >> 1) - ((fontsize * 5) / 4), ((c->pixmap_height + fontsize) >> 1) - 1);
     cairo_show_text (cr, buffer);
 
     /* check_cairo_status(cr); */
@@ -340,28 +355,68 @@ static void redraw_pixmap(CPUTempPlugin * c)
     gtk_image_set_from_pixbuf (GTK_IMAGE (c->da), pixbuf);
 }
 
-static gint get_temperature (CPUTempPlugin *th)
+static gint get_temperature (CPUTempPlugin *c)
 {
     gint max = -273, cur, i;
 
-    for (i = 0; i < th->numsensors; i++)
+    for (i = 0; i < c->numsensors; i++)
     {
-        cur = th->get_temperature[i] (th->sensor_array[i]);
+        cur = c->get_temperature[i] (c->sensor_array[i]);
         if (cur > max) max = cur;
-        th->temperature[i] = cur;
+        c->temperature[i] = cur;
     }
 
     return max;
 }
 
+#if __arm__
+static char *get_string (char *cmd)
+{
+    char *line = NULL, *res = NULL;
+    int len = 0;
+    FILE *fp = popen (cmd, "r");
+
+    if (fp == NULL) return NULL;
+    if (getline (&line, &len, fp) > 0)
+    {
+        res = line;
+        while (*res)
+        {
+            if (g_ascii_isspace (*res)) *res = 0;
+            res++;
+        }
+        res = g_strdup (line);
+    }
+    pclose (fp);
+    g_free (line);
+    return res;
+}
+
+static int get_throttle (CPUTempPlugin *c)
+{
+    char *buf;
+    unsigned int val;
+
+    buf = get_string ("vcgencmd get_throttled");
+    if (sscanf (buf, "throttled=0x%x", &val) != 1) val = 0;
+    g_free (buf);
+    return val;
+}
+#endif
+
 /* Periodic timer callback. */
-static gboolean cpu_update (CPUTempPlugin * c)
+static gboolean cpu_update (CPUTempPlugin *c)
 {
     if (g_source_is_destroyed (g_main_current_source ()))
         return FALSE;
 
     int t = get_temperature (c);
-    c->stats_cpu[c->ring_cursor] = t / 100.0; 
+    c->stats_cpu[c->ring_cursor] = t / 100.0;
+#if __arm__    
+    c->stats_throttle[c->ring_cursor] = get_throttle (c);
+#else
+    c->stats_throttle[c->ring_cursor] = 0;
+#endif
     c->ring_cursor += 1;
     if (c->ring_cursor >= c->pixmap_width) c->ring_cursor = 0;
 
@@ -383,7 +438,8 @@ static void cpu_configuration_changed (LXPanel *panel, GtkWidget *p)
         /* If statistics buffer does not exist or it changed size, reallocate and preserve existing data. */
         if ((c->stats_cpu == NULL) || (new_pixmap_width != c->pixmap_width))
         {
-            float * new_stats_cpu = g_new0 (typeof (*c->stats_cpu), new_pixmap_width);
+            float *new_stats_cpu = g_new0 (typeof (*c->stats_cpu), new_pixmap_width);
+            int *new_stats_throttle = g_new0 (typeof (*c->stats_throttle), new_pixmap_width);
             if (c->stats_cpu != NULL)
             {
                 if (new_pixmap_width > c->pixmap_width)
@@ -391,30 +447,42 @@ static void cpu_configuration_changed (LXPanel *panel, GtkWidget *p)
                     /* New allocation is larger.
                      * Introduce new "oldest" samples of zero following the cursor. */
                     memcpy (&new_stats_cpu[0],
-                        &c->stats_cpu[0], c->ring_cursor * sizeof(float));
+                        &c->stats_cpu[0], c->ring_cursor * sizeof (float));
                     memcpy (&new_stats_cpu[new_pixmap_width - c->pixmap_width + c->ring_cursor],
-                        &c->stats_cpu[c->ring_cursor], (c->pixmap_width - c->ring_cursor) * sizeof(float));
+                        &c->stats_cpu[c->ring_cursor], (c->pixmap_width - c->ring_cursor) * sizeof (float));
+                    memcpy (&new_stats_throttle[0],
+                        &c->stats_throttle[0], c->ring_cursor * sizeof (int));
+                    memcpy (&new_stats_throttle[new_pixmap_width - c->pixmap_width + c->ring_cursor],
+                        &c->stats_throttle[c->ring_cursor], (c->pixmap_width - c->ring_cursor) * sizeof (int));
                 }
                 else if (c->ring_cursor <= new_pixmap_width)
                 {
                     /* New allocation is smaller, but still larger than the ring buffer cursor.
                      * Discard the oldest samples following the cursor. */
                     memcpy (&new_stats_cpu[0],
-                        &c->stats_cpu[0], c->ring_cursor * sizeof(float));
+                        &c->stats_cpu[0], c->ring_cursor * sizeof (float));
                     memcpy (&new_stats_cpu[c->ring_cursor],
-                        &c->stats_cpu[c->pixmap_width - new_pixmap_width + c->ring_cursor], (new_pixmap_width - c->ring_cursor) * sizeof(float));
+                        &c->stats_cpu[c->pixmap_width - new_pixmap_width + c->ring_cursor], (new_pixmap_width - c->ring_cursor) * sizeof (float));
+                    memcpy (&new_stats_throttle[0],
+                        &c->stats_throttle[0], c->ring_cursor * sizeof (int));
+                    memcpy (&new_stats_throttle[c->ring_cursor],
+                        &c->stats_throttle[c->pixmap_width - new_pixmap_width + c->ring_cursor], (new_pixmap_width - c->ring_cursor) * sizeof (int));
                 }
                 else
                 {
                     /* New allocation is smaller, and also smaller than the ring buffer cursor.
                      * Discard all oldest samples following the ring buffer cursor and additional samples at the beginning of the buffer. */
                     memcpy (&new_stats_cpu[0],
-                        &c->stats_cpu[c->ring_cursor - new_pixmap_width], new_pixmap_width * sizeof(float));
+                        &c->stats_cpu[c->ring_cursor - new_pixmap_width], new_pixmap_width * sizeof (float));
+                    memcpy (&new_stats_throttle[0],
+                        &c->stats_throttle[c->ring_cursor - new_pixmap_width], new_pixmap_width * sizeof (int));
                     c->ring_cursor = 0;
                 }
                 g_free (c->stats_cpu);
+                g_free (c->stats_throttle);
             }
             c->stats_cpu = new_stats_cpu;
+            c->stats_throttle = new_stats_throttle;
         }
 
         /* Allocate or reallocate pixmap. */
@@ -479,6 +547,18 @@ static GtkWidget *cpu_constructor(LXPanel *panel, config_setting_t *settings)
             gdk_color_parse ("light gray", &c->background_color);
     } else gdk_color_parse ("light gray", &c->background_color);
 
+    if (config_setting_lookup_string (settings, "Throttle1", &str))
+    {
+        if (!gdk_color_parse (str, &c->low_throttle_color))
+            gdk_color_parse ("orange", &c->low_throttle_color);
+    } else gdk_color_parse ("orange", &c->low_throttle_color);
+
+    if (config_setting_lookup_string (settings, "Throttle2", &str))
+    {
+        if (!gdk_color_parse (str, &c->high_throttle_color))
+            gdk_color_parse ("red", &c->high_throttle_color);
+    } else gdk_color_parse ("red", &c->high_throttle_color);
+
     /* Find the system thermal sensors */
     check_sensors (c);
 
@@ -529,6 +609,10 @@ static gboolean cpu_apply_configuration (gpointer user_data)
     config_group_set_string (c->settings, "Foreground", colbuf);
     sprintf (colbuf, "%s", gdk_color_to_string (&c->background_color));
     config_group_set_string (c->settings, "Background", colbuf);
+    sprintf (colbuf, "%s", gdk_color_to_string (&c->low_throttle_color));
+    config_group_set_string (c->settings, "Throttle1", colbuf);
+    sprintf (colbuf, "%s", gdk_color_to_string (&c->high_throttle_color));
+    config_group_set_string (c->settings, "Throttle2", colbuf);
 }
 
 /* Callback when the configuration dialog is to be shown. */
@@ -539,6 +623,8 @@ static GtkWidget *cpu_configure(LXPanel *panel, GtkWidget *p)
         cpu_apply_configuration, p,
         _("Foreground colour"), &dc->foreground_color, CONF_TYPE_COLOR,
         _("Background colour"), &dc->background_color, CONF_TYPE_COLOR,
+        _("Colour when ARM frequency capped"), &dc->low_throttle_color, CONF_TYPE_COLOR,
+        _("Colour when throttled"), &dc->high_throttle_color, CONF_TYPE_COLOR,
         NULL);
 }
 
